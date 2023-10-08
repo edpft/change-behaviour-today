e4fa26f0a7e03666fa9d30f8141f242dbc904cd9
Ed Fawcett-Taylor <edwardpaultaylor@gmail.com>
2023-10-08 12:12:11 +0100

    chore: format with `prettier`

--- postcss.config.js
+++ postcss.config.js
@@ -6,28 +6,24 @@ const cssNano = require("cssnano");
 module.exports = {
     plugins: [
         autoPrefixer(),
-        cssNano(
-            {
-                preset: 'default',
-            }
-        ),
-        fontMagician(
-            {
-                variants: {
-                    "IBM Plex Sans": {
-                        '400': ['woff2'],
-                        '400 italic': ['woff2'],
-                        '700': ['woff2']
-                    },
-                    "IBM Plex Serif": {
-                        '700': ['woff2']
-                    }
+        cssNano({
+            preset: "default",
+        }),
+        fontMagician({
+            variants: {
+                "IBM Plex Sans": {
+                    400: ["woff2"],
+                    "400 italic": ["woff2"],
+                    700: ["woff2"],
                 },
-                display: 'swap',
-                foundries: ['google'],
-                protocol: 'https:',
-            }
-        ),
+                "IBM Plex Serif": {
+                    700: ["woff2"],
+                },
+            },
+            display: "swap",
+            foundries: ["google"],
+            protocol: "https:",
+        }),
         postcssPresetEnv(),
-    ]
-  }
+    ],
+};
--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,23 @@
-import { defineConfig } from 'vite'
-import viteCompression from 'vite-plugin-compression';
+import { defineConfig } from "vite";
+import viteCompression from "vite-plugin-compression";
 
 export default defineConfig({
     root: "src/",
     build: {
         outDir: "../dst/",
-        emptyOutDir: true
+        emptyOutDir: true,
     },
     preview: {
         port: 8080,
     },
     server: {
         headers: {
-            'Content-Security-Policy': "script-src 'none'; object-src 'none'; base-uri 'none';",
-          },
+            "Content-Security-Policy": "default-src 'none'; script-src 'self'; img-src 'self'; style-src 'self';",
+        },
     },
     plugins: [
         viteCompression({
-            algorithm: 'brotliCompress',
+            algorithm: "brotliCompress",
         }),
-    ]
-})
+    ],
+});
