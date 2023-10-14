a80bcb72dcab95d80942de00213cb4b234256bfd
Ed Fawcett-Taylor <edwardpaultaylor@gmail.com>
2023-10-14 22:32:07 +0100

    ch-ch-changes

--- vite.config.js
+++ vite.config.js
@@ -2,22 +2,17 @@ import { defineConfig } from "vite";
 import viteCompression from "vite-plugin-compression";
 
 export default defineConfig({
-    root: "src/",
-    build: {
-        outDir: "../dst/",
-        emptyOutDir: true,
-    },
-    preview: {
-        port: 8080,
-    },
-    server: {
-        headers: {
-            "Content-Security-Policy": "default-src 'none'; script-src 'self'; img-src 'self'; style-src 'self';",
-        },
-    },
-    plugins: [
-        viteCompression({
-            algorithm: "brotliCompress",
-        }),
-    ],
+  root: "src/",
+  build: {
+    outDir: "../dst/",
+    emptyOutDir: true,
+  },
+  preview: {
+    port: 8080,
+  },
+  plugins: [
+    viteCompression({
+      algorithm: "brotliCompress",
+    }),
+  ],
 });
